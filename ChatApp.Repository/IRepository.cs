938663f894116818259d40cde2d9d631838adee9
Jacint69 <kovacs.jacint02@gmail.com>
2024-03-19 23:17:37 +0100

    Generic Repository Interface created.

---
+++ ChatApp.Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Repository
+{
+    internal interface IRepository<T> where T : class
+    {
+        void Create(T item);
+        T Read(int id);
+        IQueryable<T> ReadAll();
+        void Update(T item);
+        void Delete(int id);
+
+    }
+}
