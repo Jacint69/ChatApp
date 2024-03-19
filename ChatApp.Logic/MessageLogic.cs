743a296335da4a3ff1d760ec932ba02a1162b792
Jacint69 <kovacs.jacint02@gmail.com>
2024-03-20 00:35:44 +0100

    Message Logic created, message length check added.

---
+++ ChatApp.Logic/MessageLogic.cs
@@ -0,0 +1,47 @@
+﻿using ChatApp.Logic.Exceptions;
+using ChatApp.Models;
+using ChatApp.Repository;
+
+namespace ChatApp.Logic
+{
+    public class MessageLogic : IMessageLogic
+    {
+
+        IRepository<Message> repository;
+        public MessageLogic(IRepository<Message> repository)
+        {
+            this.repository = repository;
+        }
+        public void Create(Message item)
+        {
+            if (item.SentMessage.Length < 1)
+            {
+                throw new LengthExceptions();
+            }
+            else
+            {
+                repository.Create(item);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            repository.Delete(id);
+        }
+
+        public Message Read(int id)
+        {
+            return repository.Read(id);
+        }
+
+        public IQueryable<Message> ReadAll()
+        {
+            return repository.ReadAll();
+        }
+
+        public void Update(Message item)
+        {
+            repository.Update(item);
+        }
+    }
+}
