516462b155c7979c5b799e0edb12ce114f2b90c4
Jacint69 <kovacs.jacint02@gmail.com>
2024-03-19 22:47:41 +0100

    MessageDbContext Created with dummy datas. Using InMemoryDatabase

--- ChatApp.Repository/MessageDbContext.cs
+++ ChatApp.Repository/MessageDbContext.cs
@@ -1,7 +1,35 @@
-﻿namespace ChatApp.Repository
+﻿using ChatApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApp.Repository
 {
-    public class MessageDbContext
+    public class MessageDbContext : DbContext
     {
+
+        public DbSet<Message> Messages {  get; set; }
+
+        public MessageDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var dummyDatas = new List<Message>()
+            {
+                new Message() {MessageId=1, Name="TestName", SentMessage="TestMessage"}
+            };
+            modelBuilder.Entity<Message>().HasData(dummyDatas);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseInMemoryDatabase("MessageDB");
+            }
+        }
+
 
     }
 }
