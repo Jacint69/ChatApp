4b807096a64935194774e7bdc31f46b58f114acf
Jacint69 <kovacs.jacint02@gmail.com>
2024-03-20 00:35:56 +0100

    MessageLogic interface extracted.

---
+++ ChatApp.Logic/IMessageLogic.cs
@@ -0,0 +1,13 @@
+﻿using ChatApp.Models;
+
+namespace ChatApp.Logic
+{
+    public interface IMessageLogic
+    {
+        void Create(Message item);
+        void Delete(int id);
+        Message Read(int id);
+        IQueryable<Message> ReadAll();
+        void Update(Message item);
+    }
+}
