c5c999f8d09969f13b5168796967290cb6dc0f5a
Jacint69 <kovacs.jacint02@gmail.com>
2024-03-20 01:02:52 +0100

    IOC container added.

--- ChatApp.Endpoint/Program.cs
+++ ChatApp.Endpoint/Program.cs
@@ -1,3 +1,7 @@
+using ChatApp.Logic;
+using ChatApp.Models;
+using ChatApp.Repository;
+
 namespace ChatApp.Endpoint
 {
     public class Program
@@ -7,6 +11,9 @@ namespace ChatApp.Endpoint
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddSingleton<MessageDbContext>();
+            builder.Services.AddTransient<IRepository<Message>, MessageRepository>();
+            builder.Services.AddTransient<IMessageLogic, MessageLogic>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
